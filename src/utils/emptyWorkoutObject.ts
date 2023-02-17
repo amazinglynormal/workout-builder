12e34c398dfefa025316ef21895515d0642b6d07
Stephen <stephenbrady365@gmail.com>
2023-02-17 19:06:43 +0000

    Add empty workout object util

---
+++ src/utils/emptyWorkoutObject.ts
@@ -0,0 +1,29 @@
+import Workout from "../interfaces/Workout.interface";
+
+const emptyWorkoutObject: Workout = {
+  numOfWorkoutDays: 1,
+  numOfWeeksToRunWorkout: 1,
+  volume: {
+    Abs: 0,
+    "Back (Horizontal)": 0,
+    "Back (Vertical)": 0,
+    Biceps: 0,
+    Calves: 0,
+    Chest: 0,
+    Delts: 0,
+    Forearms: 0,
+    Glutes: 0,
+    Hamstrings: 0,
+    Neck: 0,
+    Quads: 0,
+    Traps: 0,
+    Triceps: 0,
+  },
+  exerciseSelection: {
+    "Day #1": {
+      "1": {},
+    },
+  },
+};
+
+export default emptyWorkoutObject;
