7ddd72160181ff59c0cf33979fdbefd67d354368
Stephen <stephenbrady365@gmail.com>
2023-02-21 15:41:45 +0000

    Add testWorkoutObject in test-utils

---
+++ src/test-utils/testWorkoutObject.ts
@@ -0,0 +1,27 @@
+import Workout from "../interfacesAndTypes/Workout.interface";
+
+const testWorkoutObject: Workout = {
+  volume: {
+    Abs: 6,
+    "Back (Horizontal)": 6,
+    "Back (Vertical)": 6,
+    Biceps: 3,
+    Calves: 8,
+    Chest: 9,
+    Delts: 8,
+    Forearms: 6,
+    Glutes: 3,
+    Hamstrings: 6,
+    Neck: 0,
+    Quads: 9,
+    Traps: 0,
+    Triceps: 9,
+  },
+  exerciseSelection: {
+    "Day #1": {
+      "1": {},
+    },
+  },
+};
+
+export default testWorkoutObject;
