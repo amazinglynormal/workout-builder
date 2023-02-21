489cca98e84da90c88deed6d7865d9d2ca6c6360
Stephen <stephenbrady365@gmail.com>
2023-02-21 15:48:00 +0000

    Add tests for <VolumeCounter>

---
+++ src/volumeCounter/VolumeCounter.test.tsx
@@ -0,0 +1,23 @@
+import { render } from "@testing-library/react";
+import VolumeCounter from "./VolumeCounter";
+import testWorkoutObject from "../test-utils/testWorkoutObject";
+
+describe("<VolumeCounter>", () => {
+  test("renders correctly in the DOM", () => {
+    const { getByRole, getByText } = render(
+      <VolumeCounter workoutInfo={testWorkoutObject} />
+    );
+
+    const heading = getByRole("heading");
+    expect(heading).toBeInTheDocument();
+    expect(heading).toHaveTextContent("Volume Counter");
+
+    const paragraph = getByText(
+      "Estimate of direct sets for each muscle group. Muscles targeted by an exercise will vary with an individuals technique."
+    );
+    expect(paragraph).toBeInTheDocument();
+
+    const table = getByRole("table");
+    expect(table).toBeInTheDocument();
+  });
+});
