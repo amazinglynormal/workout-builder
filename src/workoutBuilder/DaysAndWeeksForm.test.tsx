47126c3d6f9f10a10b007c2ee8c6d34abfce78c1
Stephen <stephenbrady365@gmail.com>
2023-02-21 16:40:50 +0000

    Remove unused import

--- src/workoutBuilder/DaysAndWeeksForm.test.tsx
+++ src/workoutBuilder/DaysAndWeeksForm.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import DaysAndWeeksForm from "./DaysAndWeeksForm";
 import userEvent from "@testing-library/user-event";
 
