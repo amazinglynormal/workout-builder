211263a32a378b1a8cdb95456df4b2e4a6d2eef2
Stephen <stephenbrady365@gmail.com>
2023-03-03 19:58:06 +0000

    Add devExerciseList to utils

---
+++ src/utils/devExerciseList.ts
@@ -0,0 +1,79 @@
+import ExerciseList from "../interfacesAndTypes/ExerciseList.type";
+
+export default [
+  [
+    {
+      name: "Leg Press",
+      muscleGroup: "Quads",
+      setScheme: "Sets",
+      numOfSets: "3",
+      repScheme: "Rep Range",
+      numOfReps: "10-12",
+    },
+  ],
+  [
+    {
+      name: "Dumbbell Bench Press (Flat)",
+      muscleGroup: "Chest",
+      setScheme: "Sets",
+      numOfSets: "3",
+      repScheme: "Rep Range",
+      numOfReps: "6-10",
+    },
+    {
+      name: "Incline Dumbbell Row",
+      muscleGroup: "Back (Horizontal)",
+      setScheme: "Sets",
+      numOfSets: "3",
+      repScheme: "Rep Range",
+      numOfReps: "10-12",
+      note: "Neutral grip",
+    },
+  ],
+  [
+    {
+      name: "EZ Bar Skullcrusher",
+      muscleGroup: "Triceps",
+      setScheme: "Sets",
+      numOfSets: "3",
+      repScheme: "Rep Goal",
+      numOfReps: "45",
+    },
+    {
+      name: "Dumbbell Lateral Raise",
+      muscleGroup: "Delts",
+      setScheme: "Sets",
+      numOfSets: "3",
+      repScheme: "Rep Goal",
+      numOfReps: "45",
+    },
+  ],
+  [
+    {
+      name: "Barbell Romanian Deadlift",
+      muscleGroup: "Hamstrings",
+      setScheme: "Sets",
+      numOfSets: "3",
+      repScheme: "Rep Range",
+      numOfReps: "8-10",
+    },
+    {
+      name: "Dumbbell Curl",
+      muscleGroup: "Biceps",
+      setScheme: "Sets",
+      numOfSets: "3",
+      repScheme: "Rep Goal",
+      numOfReps: "36",
+    },
+  ],
+  [
+    {
+      name: "Barbell Wrist Curl",
+      muscleGroup: "Forearms",
+      setScheme: "Sets",
+      numOfSets: "3",
+      repScheme: "Rep Goal",
+      numOfReps: "45",
+    },
+  ],
+] as ExerciseList;
