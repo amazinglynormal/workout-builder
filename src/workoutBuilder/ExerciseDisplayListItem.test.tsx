1f71ba51fdee9542bcca2877c18203f10a24615a
Stephen <stephenbrady365@gmail.com>
2023-03-06 13:06:07 +0000

    Add <ExerciseDisplayListItem>

---
+++ src/workoutBuilder/ExerciseDisplayListItem.test.tsx
@@ -0,0 +1,145 @@
+import { act, render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import testExerciseList from "../test-utils/testExerciseList";
+import ExerciseDisplayListItem from "./ExerciseDisplayListItem";
+
+const mockDeleteExercise = jest.fn();
+
+// Test Exercise 1
+// {
+//   name: "Leg Press",
+//   muscleGroup: "Quads",
+//   setScheme: "Sets",
+//   numOfSets: "3",
+//   repScheme: "Rep Range",
+//   numOfReps: "10-12",
+// }
+
+// Test Exercise 2
+// {
+//   name: "Skiers",
+// muscleGroup: "Delts",
+// setScheme: "Sets",
+// numOfSets: "3",
+// repScheme: "Rep Goal",
+// numOfReps: "45",
+// note: "Drop set on last set",
+// },
+
+describe("<ExerciseDisplayListItem>", () => {
+  test("renders correctly in the DOM without note", () => {
+    const { getByRole, getAllByRole } = render(
+      <ol>
+        <ExerciseDisplayListItem
+          exercise={testExerciseList[0][0]}
+          index={0}
+          deleteExercise={mockDeleteExercise}
+        />
+      </ol>
+    );
+
+    const listItem = getByRole("listitem");
+    expect(listItem).toBeInTheDocument();
+    expect(listItem).toHaveTextContent("Leg Press 3 x 10-12");
+
+    const buttons = getAllByRole("button");
+    expect(buttons.length).toBe(2);
+    expect(buttons[0]).toHaveTextContent("Edit");
+    expect(buttons[1]).toHaveTextContent("Delete");
+  });
+
+  test("renders correctly in the DOM with note", () => {
+    const { getByRole, getAllByRole } = render(
+      <ol>
+        <ExerciseDisplayListItem
+          exercise={testExerciseList[2][2]}
+          index={0}
+          deleteExercise={mockDeleteExercise}
+        />
+      </ol>
+    );
+
+    const listItem = getByRole("listitem");
+    expect(listItem).toBeInTheDocument();
+    expect(listItem).toHaveTextContent("Skiers 3 x 45RG");
+    expect(listItem).not.toHaveTextContent("Drop set on last set");
+
+    const buttons = getAllByRole("button");
+    expect(buttons.length).toBe(3);
+    expect(buttons[0]).toHaveTextContent("Edit");
+    expect(buttons[1]).toHaveTextContent("Delete");
+    expect(buttons[2]).toHaveTextContent("View note");
+  });
+
+  test("renders note with 'Hide note' button when 'View note' button is clicked", () => {
+    const { getByRole, getAllByRole } = render(
+      <ol>
+        <ExerciseDisplayListItem
+          exercise={testExerciseList[2][2]}
+          index={0}
+          deleteExercise={mockDeleteExercise}
+        />
+      </ol>
+    );
+
+    let buttons = getAllByRole("button");
+    expect(buttons[2]).toHaveTextContent("View note");
+
+    act(() => userEvent.click(buttons[2]));
+
+    const listItem = getByRole("listitem");
+    expect(listItem).toHaveTextContent("Drop set on last set");
+
+    buttons = getAllByRole("button");
+    expect(buttons[2]).toHaveTextContent("Hide note");
+  });
+
+  test("renders 'View note' button when 'Hide note' button is clicked", () => {
+    const { getByRole, getAllByRole } = render(
+      <ol>
+        <ExerciseDisplayListItem
+          exercise={testExerciseList[2][2]}
+          index={0}
+          deleteExercise={mockDeleteExercise}
+        />
+      </ol>
+    );
+
+    let buttons = getAllByRole("button");
+    expect(buttons[2]).toHaveTextContent("View note");
+
+    act(() => userEvent.click(buttons[2]));
+
+    const listItem = getByRole("listitem");
+    expect(listItem).toHaveTextContent("Drop set on last set");
+
+    buttons = getAllByRole("button");
+    expect(buttons[2]).toHaveTextContent("Hide note");
+
+    act(() => userEvent.click(buttons[2]));
+
+    expect(listItem).not.toHaveTextContent("Drop set on last set");
+    buttons = getAllByRole("button");
+    expect(buttons[2]).toHaveTextContent("View note");
+  });
+
+  test("calls deleteExercise when 'Delete' button is click", () => {
+    const { getAllByRole } = render(
+      <ol>
+        <ExerciseDisplayListItem
+          exercise={testExerciseList[2][2]}
+          index={0}
+          subIndex={1}
+          deleteExercise={mockDeleteExercise}
+        />
+      </ol>
+    );
+
+    let buttons = getAllByRole("button");
+    expect(buttons[1]).toHaveTextContent("Delete");
+
+    userEvent.click(buttons[1]);
+    expect(mockDeleteExercise).toHaveBeenCalled();
+    expect(mockDeleteExercise).toHaveBeenCalledWith(0, 1);
+  });
+});
---
+++ src/workoutBuilder/ExerciseDisplayListItem.tsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import Exercise from "../interfacesAndTypes/Exercise.interface";
+import styles from "./ExerciseDisplayListItem.module.css";
+
+interface Props {
+  exercise: Exercise;
+  index: number;
+  subIndex?: number;
+  deleteExercise: (index: number, subIndex?: number) => void;
+}
+
+const ExerciseDisplayListItem = ({
+  exercise,
+  index,
+  subIndex,
+  deleteExercise,
+}: Props) => {
+  const [showNote, setShowNote] = useState(false);
+  return (
+    <li>
+      <div className={styles.listItemLayout}>
+        <div className={styles.listItemInfo}>
+          {`${exercise.name} ${exercise.numOfSets} x ${exercise.numOfReps}${
+            exercise.repScheme === "Rep Goal" ? "RG" : ""
+          }`}
+          <div>
+            <button type="button">Edit</button>
+            <button
+              type="button"
+              onClick={() => deleteExercise(index, subIndex)}
+            >
+              Delete
+            </button>
+          </div>
+        </div>
+        {exercise.note && (
+          <div className="">
+            {showNote ? (
+              <>
+                <p>{`Note: ${exercise.note}`}</p>
+                <button type="button" onClick={() => setShowNote(false)}>
+                  Hide note
+                </button>
+              </>
+            ) : (
+              <button type="button" onClick={() => setShowNote(true)}>
+                View note
+              </button>
+            )}
+          </div>
+        )}
+      </div>
+    </li>
+  );
+};
+
+export default ExerciseDisplayListItem;
