efaf5f7b0cde4523f63cfa6b9ed3941221d9feff
Stephen <stephenbrady365@gmail.com>
2023-03-27 18:02:11 +0100

    Add dropdown menu to Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,9 +12,31 @@ export default function Navbar() {
             </Link>
           </li>
           <li className={styles.listItem}>
-            <Link to="/" className={styles.link}>
-              Examples
-            </Link>
+            <div className={styles.dropdown}>
+              <button type="button" className={styles.dropdownButton}>
+                Examples
+              </button>
+              <div className={styles.dropdownContainer}>
+                <ol className={styles.dropdownMenu}>
+                  <li className={styles.dropdownListItem}>
+                    <Link
+                      to="/example/beginner3xweek"
+                      className={styles.dropdownLink}
+                    >
+                      Beginner - 3x Week
+                    </Link>
+                  </li>
+                  <li className={styles.dropdownListItem}>
+                    <Link
+                      to="/example/beginner4xweek"
+                      className={styles.dropdownLink}
+                    >
+                      Beginner - 4x Week
+                    </Link>
+                  </li>
+                </ol>
+              </div>
+            </div>
           </li>
           <li className={styles.listItem}>
             <Link to="/resources" className={styles.link}>
