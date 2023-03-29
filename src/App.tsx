69a31f1856df288ef78c4a9d3c1213188dd42cd1
Stephen <stephenbrady365@gmail.com>
2023-03-29 20:15:30 +0100

    Remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from "react";
-import ExerciseLocation from "./interfacesAndTypes/ExerciseLocation.interface";
-import { useWorkoutProgram } from "./context/WorkoutProgramContext";
 import PageLayout from "./pages/PageLayout";
 import Home from "./pages/Home";
 
