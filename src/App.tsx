bf09e50918768b9bf0967ee47aace5c0295bd556
Stephen <stephenbrady365@gmail.com>
2023-03-29 19:51:17 +0100

    Remove unused vars

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,6 @@ import PageLayout from "./pages/PageLayout";
 import Home from "./pages/Home";
 
 function App() {
-  const workoutProgram = useWorkoutProgram();
-  const [showModal, setShowModal] = useState(false);
-  const [exerciseLocation, setExerciseLocation] = useState<ExerciseLocation>({
-    day: 0,
-    index: 0,
-    subIndex: 0,
-  });
-
-  const closeModal = () => {
-    setShowModal(false);
-  };
-
-  const openModal = (exerciseLocation: ExerciseLocation) => {
-    setExerciseLocation(exerciseLocation);
-    setShowModal(true);
-  };
-
   return (
     <PageLayout>
       <Home />
